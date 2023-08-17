7445760abc48447232197eb7b636b8eb24b90b42
ertr <ertr@coinplug.com>
2023-08-17 09:52:21 +0900

    :fix: fetch content-type 추가

--- src/app/api/http.ts
+++ src/app/api/http.ts
@@ -1,7 +1,12 @@
 import { NextResponse } from "next/server";
 
 export async function http<T>(request: RequestInfo): Promise<T> {
-  const response = await fetch(request);
+  const response = await fetch(request, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   const data = await response.json();
 
   return data;

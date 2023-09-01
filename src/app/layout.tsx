bacc30c26c78e7a7ac56e7fcd6393aa361de4353
ertr <ertr@coinplug.com>
2023-09-01 16:47:33 +0900

    :fix: layout 콘솔 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   const cookieStore = cookies();
   const darkCookie = cookieStore.get("darkMode");
   const initDark = darkCookie ? darkCookie.value === "true" : false;
-
+  console.log(433, darkCookie, initDark);
   return (
     <html lang="en" className={initDark ? "dark" : ""}>
       <body className="dark:bg-black ">

bfcea20e890b8791fb7a1042b1be666fbbc50858
최진 <gogleset@choejin-ui-MacBookPro.local>
2023-09-04 15:48:44 +0900

    feature: set ssr darkMode -3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import type { Metadata } from "next";
-import { ssrDarkModeFn } from "@/utils/cookie";
-import Script from "next/script";
+import { cookies } from "next/headers";
 
 export const metadata: Metadata = {
   title: "Coin-MarketInfo-Sheet",
@@ -10,22 +9,20 @@ export const metadata: Metadata = {
     "A site that retrieves the coin information from the exchange and extracts the sheet file.",
 };
 
-const ScriptTag = () => {
-  const stringifyFn = String(ssrDarkModeFn);
-
-  const fnToRunOnClient = `(${stringifyFn})()`;
-  return <script dangerouslySetInnerHTML={{ __html: fnToRunOnClient }} />;
-};
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const theme = cookies().get("darkMode");
+  console.log(theme);
+
+  function getThemeClass() {
+    return theme ? (theme.value === "true" ? "dark" : "") : "";
+  }
   return (
-    <html lang='en' id='HTML' suppressHydrationWarning={true}>
+    <html lang='en' className={getThemeClass()}>
       <body className='dark:bg-black '>
-        <ScriptTag />
         <Header />
         {children}
       </body>
